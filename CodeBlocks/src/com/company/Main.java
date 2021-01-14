b669eeba2131a6e1ae624761de2c3b003a2d715f
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-01-14 19:53:28 +0100

    methods

--- CodeBlocks/src/com/company/Main.java
+++ CodeBlocks/src/com/company/Main.java
@@ -3,26 +3,21 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    boolean gameOver = true;
-	    int score = 800;
-	    int levelCompleted = 5;
-	    int bonus = 10;
-
-	    if (score < 5000 && score > 1000) {
-			System.out.println("Your score was less than 5000 but greater than 1000");
-		} else if (score < 1000) {
-			System.out.println("Your score is less than 1000");
-		} else {
-			System.out.println("Got here");
-		}
-
-	    if (gameOver) {
-	    	int finalScore = score + (levelCompleted * bonus);
-			System.out.println("Your final Score is " + finalScore);
-		}
-
-	    // int totalFinalScore = finalScore; This is not allowed, since finalScore was created within the "If" block
-
-		System.out.println("this was executed");
+    	calculateStore(true,800, 5, 100 );
+    	calculateStore(true, 10000, 8, 200);
     }
+
+    public static int calculateStore(
+    		boolean gameOver,
+			int score,
+			int levelCompleted,
+			int bonus) {
+		if (gameOver) {
+			int finalScore = score + (levelCompleted * bonus);
+			finalScore += 1000;
+			System.out.println("Your final Score is " + finalScore);
+			return finalScore;
+		}
+		return -1;
+	}
 }
