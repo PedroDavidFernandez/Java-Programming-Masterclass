32f5c89e6bf42bdf42dc48659b4036453bc94d18
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-01-17 13:45:48 +0100

    code exercise 7

--- 07_Coding_Exercises_Teen_Number_Check/src/com/company/TeenNumberChecker.java
+++ 07_Coding_Exercises_Teen_Number_Check/src/com/company/TeenNumberChecker.java
@@ -3,6 +3,30 @@ package com.company;
 public class TeenNumberChecker {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(hasTeen(1, 6, 7)); // return false
+        System.out.println(hasTeen(9, 99, 19)); // return true
+        System.out.println(hasTeen(23, 15, 52)); // return true
+        System.out.println(hasTeen(22, 23, 24)); // return false
+        System.out.println(isTeen(12)); // return false
+        System.out.println(isTeen(13)); // return true
+        System.out.println(isTeen(17)); // return true
+        System.out.println(isTeen(19)); // return true
+        System.out.println(isTeen(20)); // return false
+    }
+
+    public static boolean hasTeen(int age1, int age2, int age3) {
+        int[] ages = {age1, age2, age3};
+
+        for (int age : ages) {
+            if (isTeen(age)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean isTeen(int age) {
+        return age >= 13 && age <= 19;
     }
 }
