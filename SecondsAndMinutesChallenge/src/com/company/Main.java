25ff9fe3fe7307190a0588149776839d9187d1a1
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-01-21 08:05:55 +0100

    format hours less than 10

--- SecondsAndMinutesChallenge/src/com/company/Main.java
+++ SecondsAndMinutesChallenge/src/com/company/Main.java
@@ -1,7 +1,10 @@
 package com.company;
 
+import java.text.DecimalFormat;
+
 public class Main {
     public static final int SIXTY = 60;
+    public static final String INVALID_MESSAGE = "Invalid value";
 
     public static void main(String[] args) {
         System.out.println("*** call getDurationString(int minutes, int seconds) *** \n");
@@ -11,7 +14,11 @@ public class Main {
         System.out.println(getDurationString(150, 25));
         System.out.println(getDurationString(120, 10));
         System.out.println(getDurationString(90, 0));
-        System.out.println(getDurationString(60, 0));
+        System.out.println(getDurationString(61, 0));
+        System.out.println(getDurationString(120, 0));
+        System.out.println(getDurationString(180, 0));
+        System.out.println(getDurationString(559, 0));
+        System.out.println(getDurationString(600, 0));
         System.out.println(getDurationString(30, 0));
         System.out.println(getDurationString(20, 0));
         System.out.println(getDurationString(5, 5));
@@ -33,11 +40,17 @@ public class Main {
 
     public static String getDurationString(int minutes, int seconds) {
         if (minutes < 0 || ((seconds < 0) || (seconds > 59))) {
-            return "Invalid value";
+            return INVALID_MESSAGE;
         }
 
         int remainingMinutes = minutes % SIXTY;
         int hours = minutes / SIXTY;
+
+        if (hours < 10) {
+            String paddingHour = String.format("%02d", hours);
+            String paddingMinutes = String.format("%02d", remainingMinutes);
+            return paddingHour + "h " + paddingMinutes + "m " + seconds + "s";
+        }
 
         return hours + "h " + remainingMinutes + "m " + seconds + "s";
     }
