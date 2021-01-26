884fd07f2478af7ffaa0f972d280e74df79f8fe5
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-01-26 08:22:25 +0100

    While and Do While challenge

--- WhileAndDoWhileStatements/src/com/company/Main.java
+++ WhileAndDoWhileStatements/src/com/company/Main.java
@@ -3,44 +3,31 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    int count = 0;
-	    while(count != 5) {
-            System.out.println("Count value is " + count);
-            count++;
-        }
-
-	    count = 1;
-        while(true) {
-            if (count == 4) {
-                break;
-            }
-            System.out.println("** Count value is " + count);
-            count++;
-        }
-
-        count = 6;
-        do {
-            System.out.println("^^ Count value was " + count);
-            count++;
-
-            if(count > 100) {
-                System.out.println("breaking at 100");
-                break;
-            }
-
-        } while (count != 6);
-
-        //System.out.println(isEvenNumber(6));
-
         int number = 4;
         int finishNumber = 20;
+        int count = 0;
+        int sum = 0;
         while (number <= finishNumber) {
             number++;
             if (!isEvenNumber(number)) {
                 continue;
             }
+            count++;
+            sum++;
+
+            if (count > 5) {
+                break;
+            }
+
             System.out.println("Even number is: " + number);
+            System.out.println("total number of even numbers: " + sum);
+            //System.out.println("count is " + count);
         }
+
+        // modify the code above
+        // make it also to record the total number of even numbers it has found
+        // and break once 5 are found
+        // and at the end display the total number of even numbers found
     }
 
     private static boolean isEvenNumber(int number) {
