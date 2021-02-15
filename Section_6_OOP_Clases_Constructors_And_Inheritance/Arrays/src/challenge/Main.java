789609c6248da3eae61c206e45eaa920a716dec5
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-02-15 17:30:21 +0100

    solution with while loop

--- Section_6_OOP_Clases_Constructors_And_Inheritance/Arrays/src/challenge/Main.java
+++ Section_6_OOP_Clases_Constructors_And_Inheritance/Arrays/src/challenge/Main.java
@@ -7,12 +7,32 @@ public class Main {
     public static void main(String[] args) {
         // implement the following methods getIntegers(), printArray(), sortIntegers()
 
-        int[] array = getIntegers(2);
+        int[] array = getIntegers(3);
         int[] sortedArray = sortIntegers(array);
         printArray(sortedArray);
     }
 
     private static int[] sortIntegers(int[] array) {
+        int[] sortedArray = new int[array.length];
+        for (int i=0; i < array.length; i++) {
+            sortedArray[i] = array[i];
+        }
+        boolean flag = true;
+        int temp;
+        while (flag) {
+            flag = false;
+            for (int i=0; i < sortedArray.length - 1; i++) {
+                if (sortedArray[i] < sortedArray[i +1]) {
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i + 1];
+                    sortedArray[i + 1] = temp;
+                }
+            }
+        }
+        return sortedArray;
+    }
+
+    /*private static int[] sortIntegers(int[] array) {
         int temp;
         for (int i=0; i < array.length; i++) {
             for (int j=0; j < array.length; j++) {
@@ -24,8 +44,7 @@ public class Main {
             }
         }
         return array;
-    }
-
+    }*/
     private static void printArray(int[] integers) {
         System.out.println("Values from array: ");
         for(int i = 0; i < integers.length; i++) {
