fc1702926ee0b2adc9932e8fbfc7142d22c3d511
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-02-16 09:57:26 +0100

    sorted array execise

---
+++ __Coding_Exercises/25_Coding_Exercise_Sorted_Array/src/com/company/SortedArray.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class SortedArray {
+
+    private static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
