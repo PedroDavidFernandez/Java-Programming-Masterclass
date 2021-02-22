b144caca61de41bc6ff856f6f232732b8e7eea4b
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-02-22 08:19:09 +0100

    minimum element challenge - take 2

--- __Coding_Exercises/26_Coding_Exercise_Minimum_Element_Challenge/src/com/company/MinimumElement.java
+++ __Coding_Exercises/26_Coding_Exercise_Minimum_Element_Challenge/src/com/company/MinimumElement.java
@@ -1,35 +1,39 @@
 package com.company;
 
+
 import java.util.Scanner;
 
 public class MinimumElement {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-	    int[] arrayOfIntegers = readIntegers(5);
-
-
-        int min = findMin(arrayOfIntegers);
-        System.out.println("Min Value is: " + min);
+        int numberOfIntegers = readInteger();
+        int[] array = readElements(numberOfIntegers);
+        int value = findMin(array);
+        System.out.println("Minimum value is: " + value);
     }
 
-    private static int[] readIntegers(int numberOfIntegers) {
-        System.out.println("Please enter 6 numbers: ");
-        int[] array = new int[numberOfIntegers];
+    private static int readInteger() {
+        System.out.println("Please enter the number of integers you want to extract minim value:");
+        return scanner.nextInt();
+    }
+
+    private static int[] readElements(int numberOfIntegers) {
+        System.out.println("Please enter " + numberOfIntegers + " numbers.");
+        int[] arrayOfIntegers = new int[numberOfIntegers];
         for (int i=0; i < numberOfIntegers; i++) {
-            array[i] = scanner.nextInt();
+            arrayOfIntegers[i] = scanner.nextInt();
         }
 
-        return array;
+        return arrayOfIntegers;
     }
 
-    private static int findMin(int[] arrayOfIntegers) {
+    private static int findMin(int[] array) {
         int minValue = Integer.MAX_VALUE;
-        for (int i=0; i < arrayOfIntegers.length; i++) {
-            int value = arrayOfIntegers[i];
 
-            if (value < minValue) {
-                minValue = value;
+        for (int i=0; i < array.length; i++) {
+            if (array[i] < minValue) {
+                minValue = array[i];
             }
         }
         return minValue;
