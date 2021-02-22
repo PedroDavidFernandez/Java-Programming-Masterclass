bbc35e7152e0b0a090827645796a8b6dd4305184
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-02-22 08:56:51 +0100

    solution to reverse array swapping elements

--- __Coding_Exercises/27_Coding_Exercise_Reverse_Array/src/com/company/ReverseArray.java
+++ __Coding_Exercises/27_Coding_Exercise_Reverse_Array/src/com/company/ReverseArray.java
@@ -1,20 +1,25 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class ReverseArray {
 
     public static void main(String[] args) {
-	    reverese(new int[]{1,2,3,4,5});
+        int[] array = new int[]{10,20,30,40,50};
+        System.out.println("Array: " + Arrays.toString(array));
+
+        reverse(array);
+        System.out.println("Reversed Array: " + Arrays.toString(array));
     }
 
-    private static void reverese(int[] array) {
-        System.out.println("Array");
-        for (int i=0; i < array.length; i++) {
-            System.out.println(array[i]);
-        }
+    private static void reverse(int[] array) {
+        int maxIndex = array.length - 1;
+        int halfLength = array.length / 2;
 
-        System.out.println("Reversed Array");
-        for (int i=array.length - 1; i < array.length; i--) {
-            System.out.println(array[i]);
+        for (int i=0; i < halfLength; i++) {
+            int temp = array[i];
+            array[i] = array[maxIndex - i];
+            array[maxIndex - i] = temp;
         }
     }
 }
