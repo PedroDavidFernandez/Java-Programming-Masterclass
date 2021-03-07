79b89b87e01f788b5453a27c762bebe74624db4b
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-03-07 13:09:52 +0100

    Simple Bank Application Challenge - implement test code

--- Section_7_Arrays/Arrays/src/List_ArrayList/Autoboxing_Unboxing_Challenge_version2/Main.java
+++ Section_7_Arrays/Arrays/src/List_ArrayList/Autoboxing_Unboxing_Challenge_version2/Main.java
@@ -1,4 +1,21 @@
 package Autoboxing_Unboxing_Challenge_version2;
 
 public class Main {
+    public static void main(String[] args) {
+        Bank bank = new Bank("National Australia Bank");
+        bank.addBranch("Adelaide");
+        bank.addCustomer("Adelaide", "Tim", 50.05);
+        bank.addCustomer("Adelaide", "Mike", 75.34);
+        bank.addCustomer("Adelaide", "Percy", 220.12);
+
+        bank.addBranch("Sidney");
+        bank.addCustomer("Sidney", "Bob", 150.54);
+
+        bank.addCustomerTransaction("Adelaide", "Tim", 44.22);
+        bank.addCustomerTransaction("Adelaide", "Tim", 12.44);
+        bank.addCustomerTransaction("Adelaide", "Mike", 1.65);
+
+        bank.listCustomers("Adelaide", false);
+        bank.listCustomers("Adelaide", true);
+    }
 }
