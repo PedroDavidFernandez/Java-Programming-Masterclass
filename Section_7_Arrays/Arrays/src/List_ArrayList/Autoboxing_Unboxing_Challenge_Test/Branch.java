f5ffca6cd01617dfe9fc7d5c67a1662417fbf694
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-03-10 06:57:16 +0100

    Simple Bank Application Challenge - loop

---
+++ Section_7_Arrays/Arrays/src/List_ArrayList/Autoboxing_Unboxing_Challenge_Test/Branch.java
@@ -0,0 +1,51 @@
+package Autoboxing_Unboxing_Challenge_Test;
+
+import java.util.ArrayList;
+
+public class Branch {
+    private String name;
+    private ArrayList<Customer> customers;
+
+    public Branch(String name) {
+        this.name = name;
+        this.customers = new ArrayList<Customer>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<Customer> getCustomers() {
+        return customers;
+    }
+
+    public boolean addCustomer(String name, double initialTransaction) {
+        if (findCustomer(name) == null) {
+            this.customers.add(new Customer(name, initialTransaction));
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean addCustomerTransaction(String name, double amount) {
+        Customer existingCustomer = findCustomer(name);
+        if (existingCustomer != null) {
+            existingCustomer.addTransaction(amount);
+            return true;
+        }
+
+        return false;
+    }
+
+    private Customer findCustomer(String name) {
+        for (int i=0; i<this.customers.size(); i++) {
+            Customer checkedCustomer = this.customers.get(i);
+            if (checkedCustomer.getName().equals(name)) {
+                return checkedCustomer;
+            }
+        }
+
+        return null;
+    }
+}
