afbfd3a91bd69051b03cd2ab0097aa664c5bad50
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-03-23 15:51:52 +0100

    interface basic example

---
+++ Section_9_Inner_and_Abstract_Classes_Interfaces/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    ITelephone pedroPhone;
+	    pedroPhone = new DeskPhone(1234567);
+	    pedroPhone.powerOn();
+	    pedroPhone.callPhone(1234567);
+	    pedroPhone.answer();
+    }
+}
