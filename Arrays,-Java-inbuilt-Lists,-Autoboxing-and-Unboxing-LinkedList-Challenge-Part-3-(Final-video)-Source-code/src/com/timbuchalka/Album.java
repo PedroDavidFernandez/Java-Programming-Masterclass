34ed7d6624a11fb3631c5e53a989df49e6c9fe88
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-03-28 11:48:31 +0200

    inner class test 2

--- Arrays,-Java-inbuilt-Lists,-Autoboxing-and-Unboxing-LinkedList-Challenge-Part-3-(Final-video)-Source-code/src/com/timbuchalka/Album.java
+++ Arrays,-Java-inbuilt-Lists,-Autoboxing-and-Unboxing-LinkedList-Challenge-Part-3-(Final-video)-Source-code/src/com/timbuchalka/Album.java
@@ -9,35 +9,22 @@ import java.util.LinkedList;
 public class Album {
     private String name;
     private String artist;
-    private ArrayList<Song> songs;
+    private SongList songs;
 
     public Album(String name, String artist) {
         this.name = name;
         this.artist = artist;
-        this.songs = new ArrayList<Song>();
+        this.songs = new SongList();
     }
 
     public boolean addSong(String title, double duration) {
-        if(findSong(title) == null) {
-            this.songs.add(new Song(title, duration));
-            return true;
-        }
-        return false;
-    }
-
-    private Song findSong(String title) {
-        for(Song checkedSong: this.songs) {
-            if(checkedSong.getTitle().equals(title)) {
-                return checkedSong;
-            }
-        }
-        return null;
+        return this.songs.add(new Song(title, duration));
     }
 
     public boolean addToPlayList(int trackNumber, LinkedList<Song> playList) {
-        int index = trackNumber -1;
-        if((index >0) && (index <= this.songs.size())) {
-            playList.add(this.songs.get(index));
+        Song checkedSong = this.songs.findSong(trackNumber);
+        if(checkedSong != null) {
+            playList.add(checkedSong);
             return true;
         }
         System.out.println("This album does not have a track " + trackNumber);
@@ -45,7 +32,7 @@ public class Album {
     }
 
     public boolean addToPlayList(String title, LinkedList<Song> playList) {
-        Song checkedSong = findSong(title);
+        Song checkedSong = this.songs.findSong(title);
         if(checkedSong != null) {
             playList.add(checkedSong);
             return true;
@@ -54,22 +41,37 @@ public class Album {
         return false;
     }
 
+    public class SongList {
+        private ArrayList<Song> songs;
 
+        private SongList() {
+            this.songs = new ArrayList<Song>();
+        }
 
+        private boolean add(Song song) {
+            if (this.songs.contains(song)) {
+                return false;
+            }
 
+            this.songs.add(song);
+            return true;
+        }
 
+        private Song findSong(String title) {
+            for(Song checkedSong: this.songs) {
+                if(checkedSong.getTitle().equals(title)) {
+                    return checkedSong;
+                }
+            }
+            return null;
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+        private Song findSong(int trackNumber) {
+            int index = trackNumber - 1;
+            if((index >= 0) && (index<this.songs.size())) {
+                return this.songs.get(index);
+            }
+            return null;
+        }
+    }
 }
