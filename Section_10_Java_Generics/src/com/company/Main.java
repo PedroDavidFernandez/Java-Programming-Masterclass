6158e44f0f33f98480363caccc02d1ff17a91b55
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-03-30 08:19:43 +0200

    Generics - part 2

--- Section_10_Java_Generics/src/com/company/Main.java
+++ Section_10_Java_Generics/src/com/company/Main.java
@@ -1,19 +1,32 @@
 package com.company;
 
-import java.util.ArrayList;
-
 public class Main {
 
     public static void main(String[] args) {
-        FootballPlayer player = new FootballPlayer("Benzema");
-        BasketballPlayer player1 = new BasketballPlayer("LeBron");
-        HandballPlayer player2 = new HandballPlayer("Niko");
+        FootballPlayer karim = new FootballPlayer("Benzema");
+        FootballPlayer grizzman = new FootballPlayer("Antoine");
+        BasketballPlayer james = new BasketballPlayer("LeBron");
+        BasketballPlayer david = new BasketballPlayer("Antony Davis");
+        BasketballPlayer gasol = new BasketballPlayer("Marc Gasol");
+        HandballPlayer klauss = new HandballPlayer("Niko");
 
-        Team team1 = new Team("Caledonian Team");
-        team1.addPlayer(player);
-        team1.addPlayer(player1);
-        team1.addPlayer(player2);
+        Team<FootballPlayer> nationalTeam = new Team("France");
+        nationalTeam.addPlayer(karim);
+        nationalTeam.addPlayer(grizzman);
+        //nationalTeam.addPlayer(james);
+        //nationalTeam.addPlayer(klauss);
 
-        System.out.println(team1.numPlayers());
+        System.out.println(nationalTeam.numPlayers());
+
+        Team<BasketballPlayer> lakers = new Team<>("Los Angeles Lakers");
+        lakers.addPlayer(james);
+        lakers.addPlayer(david);
+        lakers.addPlayer(gasol);
+
+        System.out.println(lakers.numPlayers());
+
+        //Team<String> brokenTeam = new Team<>("this wont work");
+        //brokenTeam.addPlayer("no one");
+
     }
 }
