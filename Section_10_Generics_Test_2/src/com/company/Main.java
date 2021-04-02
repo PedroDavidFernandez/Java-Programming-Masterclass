8cc5221bc3bca3f57d7d333031be7fda001ada0f
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-04-02 16:31:00 +0200

    generics - team class methods for match result and ranking + tests on main class

--- Section_10_Generics_Test_2/src/com/company/Main.java
+++ Section_10_Generics_Test_2/src/com/company/Main.java
@@ -3,6 +3,47 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    FootballPlayer kun = new FootballPlayer("Kun Agüero");
+	    FootballPlayer mbappe = new FootballPlayer("Kylian Mbappé");
+	    FootballPlayer dybala = new FootballPlayer("Paulo Dybala");
+
+	    FootballPlayer ramos = new FootballPlayer("Sergio Ramos");
+	    FootballPlayer modric = new FootballPlayer("Luka Modric");
+	    FootballPlayer karim = new FootballPlayer("Karim Benzema");
+	    
+	    AmericanfootballPlayer rice = new AmericanfootballPlayer("Jerry Rice");
+	    AmericanfootballPlayer brady = new AmericanfootballPlayer("Tom Brady");
+	    AmericanfootballPlayer mahomes = new AmericanfootballPlayer("Patrick Mahomes");
+
+	    Team<AmericanfootballPlayer> patriots = new Team<>("New England Patriots");
+		patriots.addPlayer(rice);
+		patriots.addPlayer(brady);
+		patriots.addPlayer(mahomes);
+
+	    Team<FootballPlayer> galaxy = new Team<>("LA Galaxy");
+	    galaxy.addPlayer(kun);
+	    galaxy.addPlayer(kun);
+	    galaxy.addPlayer(mbappe);
+	    galaxy.addPlayer(dybala);
+//	    galaxy.addPlayer(rice);
+//	    galaxy.addPlayer(brady);
+
+		Team<FootballPlayer> real = new Team<>("Real Madrid");
+		real.addPlayer(ramos);
+		real.addPlayer(modric);
+		real.addPlayer(karim);
+
+//		System.out.println("Number of players on team "
+//				+ galaxy.getName() + " is: " + galaxy.getNumberOfPlayers());
+
+		real.matchResult(galaxy, 3, 1);
+		real.matchResult(galaxy, 2, 0);
+		real.matchResult(galaxy, 1, 2);
+		galaxy.matchResult(real, 2, 1);
+		galaxy.matchResult(real, 3, 4);
+		galaxy.matchResult(real, 1, 1);
+
+		System.out.println(real.getName() + " Ranking is: " + real.ranking() + "\n");
+		System.out.println(galaxy.getName()  + " Ranking is: " + galaxy.ranking() + "\n");
     }
 }
--- Section_10_Generics_Test_2/src/com/company/Team.java
+++ Section_10_Generics_Test_2/src/com/company/Team.java
@@ -2,14 +2,14 @@ package com.company;
 
 import java.util.ArrayList;
 
-public class Team {
+public class Team<T extends Player> {
     private String name;
     private int won;
     private int tied;
     private int lost;
     private int played;
 
-    private ArrayList teams = new ArrayList();
+    private ArrayList<T> teams = new ArrayList();
 
     public Team(String name) {
         this.name = name;
@@ -18,5 +18,36 @@ public class Team {
     public String getName() {
         return name;
     }
-    
+
+    public boolean addPlayer(T player) {
+        if (this.teams.contains(player)) {
+            System.out.println(player.getName() + " has been already picked by the team!\n");
+            return false;
+        }
+        this.teams.add(player);
+        System.out.println(player.getName() + " has been picked by the team\n");
+        return true;
+    }
+
+    public int getNumberOfPlayers() {
+        return this.teams.size();
+    }
+
+    public void matchResult(Team opponent, int ourScore, int theirScore) {
+        if (ourScore>theirScore) {
+            won++;
+        } else if (ourScore==theirScore) {
+            tied++;
+        } else {
+            lost++;
+        }
+        played++;
+        if(opponent!=null) {
+            matchResult(null, ourScore, theirScore);
+        }
+    }
+
+    public int ranking() {
+        return (won * 3) + tied;
+    }
 }
