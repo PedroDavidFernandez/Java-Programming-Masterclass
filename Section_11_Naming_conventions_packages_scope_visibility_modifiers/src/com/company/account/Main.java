1357d2a75d9ab3dc8e1590717dd4992d47e0febe
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-04-08 09:21:27 +0200

    access modifiers - part 1

---
+++ Section_11_Naming_conventions_packages_scope_visibility_modifiers/src/com/company/account/Main.java
@@ -0,0 +1,13 @@
+package com.company.account;
+
+public class Main {
+    public static void main(String[] args) {
+        Account pedroAccount = new Account("Pedro");
+        pedroAccount.deposit(1000);
+        pedroAccount.withdraw(500);
+        pedroAccount.deposit(-20);
+        pedroAccount.withdraw(-10);
+        pedroAccount.deposit(300);
+        pedroAccount.calculateBalance();
+    }
+}
