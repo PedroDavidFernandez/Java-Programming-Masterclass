7551b2782f714ebfd2c59848efe69c5cd3a3209c
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-04-24 19:15:42 +0200

    maps - fix warnings

--- Section_11_Maps/src/com/company/adventure/Driver.java
+++ Section_11_Maps/src/com/company/adventure/Driver.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Driver {
-    private static Map<Integer, Location> locations = new HashMap<Integer, Location>();
+    private static final Map<Integer, Location> locations = new HashMap<>();
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         locations.put(0, new Location(0, "You are sitting in front of the computer learning Java"));
