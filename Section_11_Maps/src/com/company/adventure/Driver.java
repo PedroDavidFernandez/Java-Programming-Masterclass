4c616cf577e2d8814d8e6c27dafa1c620fca19c4
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-04-25 12:40:49 +0200

    maps - remove unused import

--- Section_11_Maps/src/com/company/adventure/Driver.java
+++ Section_11_Maps/src/com/company/adventure/Driver.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class Driver {
     private static final Map<Integer, Location> locations = new HashMap<>();
