3392e25adb47630520a7b1afa793edbb2cd0cffc
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-04-25 12:39:28 +0200

    maps - challenge 2

--- Section_11_Maps/src/com/company/adventure/Driver.java
+++ Section_11_Maps/src/com/company/adventure/Driver.java
@@ -18,30 +18,26 @@ public class Driver {
         locations.put(5, new Location(5, "You are in the forest"));
 
         locations.get(1).addExit("W", 2);
-        locations.get(1).addExit("WEST", 2);
         locations.get(1).addExit("E", 3);
-        locations.get(1).addExit("EAST", 3);
         locations.get(1).addExit("S", 4);
-        locations.get(1).addExit("SOUTH", 4);
         locations.get(1).addExit("N", 5);
 
         locations.get(2).addExit("N", 5);
-        locations.get(2).addExit("NORTH", 5);
 
         locations.get(3).addExit("W", 1);
-        locations.get(3).addExit("WEST", 1);
 
         locations.get(4).addExit("N", 1);
-        locations.get(4).addExit("NORTH", 1);
         locations.get(4).addExit("W", 2);
-        locations.get(4).addExit("WEST", 2);
 
         locations.get(5).addExit("S", 1);
-        locations.get(5).addExit("SOUTH", 1);
         locations.get(5).addExit("W", 2);
-        locations.get(5).addExit("WEST", 2);
 
-        System.out.println(locations.get(1).getExits());
+        Map<String, String> vocabulary = new HashMap<String, String>();
+        vocabulary.put("QUIT", "Q");
+        vocabulary.put("NORTH", "N");
+        vocabulary.put("SOUTH", "S");
+        vocabulary.put("WEST", "W");
+        vocabulary.put("EAST", "E");
 
         int loc = 1;
         while (true) {
@@ -53,18 +49,25 @@ public class Driver {
             Map<String, Integer> exits = locations.get(loc).getExits();
             System.out.print("Available exits are: ");
             for (String exit: exits.keySet()) {
-                if (exit.equals("W") || exit.equals("E") || exit.equals("N") || exit.equals("S")) {
-                    System.out.print(exit + ", ");
-                }
+                System.out.print(exit + ", ");
             }
             System.out.println();
 
-            String directions = scanner.nextLine().toUpperCase(Locale.ROOT);
-            String[] splitDirection = directions.split(" ");
-            for (String direction : splitDirection) {
-                if (exits.containsKey(direction)) {
-                    loc = exits.get(direction);
+            String direction = scanner.nextLine().toUpperCase(Locale.ROOT);
+            if (direction.length() > 1) {
+                String[] words = direction.split(" ");
+                for (String word : words) {
+                    if (vocabulary.containsKey(word)) {
+                        direction = vocabulary.get(word);
+                        break;
+                    }
                 }
+            }
+
+            if (exits.containsKey(direction)) {
+                loc = exits.get(direction);
+            } else {
+                System.out.println("You cannot go in that direction!");
             }
         }
     }
