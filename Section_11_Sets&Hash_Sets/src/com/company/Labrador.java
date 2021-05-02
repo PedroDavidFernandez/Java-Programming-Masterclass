ad68c7c69c8cfee1a2e7acf10284bd86947c065f
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-05-02 11:38:39 +0200

    HashSet - potential issue with equals() and sub-classing - part 2

--- Section_11_Sets&Hash_Sets/src/com/company/Labrador.java
+++ Section_11_Sets&Hash_Sets/src/com/company/Labrador.java
@@ -5,18 +5,18 @@ public class Labrador extends Dog{
         super(name);
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if(this == obj) {
-            return true;
-        }
-
-        System.out.println("From Labrador class - obj.getClass() is: " + obj.getClass());
-        if(obj instanceof Labrador) {
-            String objName = ((Labrador) obj).getName();
-            return this.getName().equals(objName);
-        }
-
-        return false;
-    }
+//    @Override
+//    public boolean equals(Object obj) {
+//        if(this == obj) {
+//            return true;
+//        }
+//
+//        System.out.println("From Labrador class - obj.getClass() is: " + obj.getClass());
+//        if(obj instanceof Labrador) {
+//            String objName = ((Labrador) obj).getName();
+//            return this.getName().equals(objName);
+//        }
+//
+//        return false;
+//    }
 }
