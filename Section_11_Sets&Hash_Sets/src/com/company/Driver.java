f3616e8d974715a7f7099948797cc38552c87744
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-05-09 18:05:27 +0200

    Sets - pracice 4

--- Section_11_Sets&Hash_Sets/src/com/company/Driver.java
+++ Section_11_Sets&Hash_Sets/src/com/company/Driver.java
@@ -6,57 +6,56 @@ import java.util.Map;
 import java.util.Set;
 
 public class Driver {
-    private static final Map<String, HeavenlyBody> solarSystem = new HashMap<>();
+    private static final Map<HeavenlyBody.Key, HeavenlyBody> solarSystem = new HashMap<>();
     private static final Set<HeavenlyBody> planets = new HashSet<>();
 
     public static void main(String[] args) {
         HeavenlyBody temp = new Planet("Saturn", 10.747);
-        solarSystem.put(temp.getName(), temp);
+        solarSystem.put(temp.getKey(), temp);
         planets.add(temp);
 
         temp = new Planet("Mars", 687.0);
-        solarSystem.put(temp.getName(), temp);
+        solarSystem.put(temp.getKey(), temp);
         planets.add(temp);
 
         HeavenlyBody tempMoon = new Moon("Dheimos", 3);
-        solarSystem.put(tempMoon.getName(), tempMoon);
+        solarSystem.put(tempMoon.getKey(), tempMoon);
         temp.addSatellites(tempMoon);
 
         tempMoon = new Moon("Phobos", 1);
-        solarSystem.put(tempMoon.getName(), tempMoon);
+        solarSystem.put(tempMoon.getKey(), tempMoon);
         temp.addSatellites(tempMoon);
 
         temp = new Planet("Jupiter", 4331);
-        solarSystem.put(temp.getName(), temp);
+        solarSystem.put(temp.getKey(), temp);
         planets.add(temp);
 
         temp = new Planet("Earth", 365.2);
-        solarSystem.put(temp.getName(), temp);
+        solarSystem.put(temp.getKey(), temp);
         planets.add(temp);
 
         tempMoon = new Moon("Moon", 27.3);
-        solarSystem.put(tempMoon.getName(), tempMoon);
+        solarSystem.put(tempMoon.getKey(), tempMoon);
         temp.addSatellites(tempMoon);
 
         temp = new Planet("Pluto", 90.560);
-        solarSystem.put(temp.getName(), temp);
+        solarSystem.put(temp.getKey(), temp);
         planets.add(temp);
 
         // duplicated Pluto
         HeavenlyBody pluto = new DwarfPlanet("Pluto", 97.560);
-        solarSystem.put(pluto.getName(), pluto);
+        solarSystem.put(pluto.getKey(), pluto);
         planets.add(pluto);
 
         System.out.println("Solar system planets:");
         for (HeavenlyBody planet : planets) {
-            System.out.println("\t" + planet.toString());
+                System.out.println("\t" + planet.toString());
         }
 
         System.out.println("Moons of Mars:");
-        HeavenlyBody body = solarSystem.get("Mars");
-        for (HeavenlyBody moonsOfMars : body.getSatellites()) {
-            System.out.println("\t" + moonsOfMars);
-        }
+        HeavenlyBody body = solarSystem.get(
+                HeavenlyBody.makeKey("Jupiter", HeavenlyBody.BodyTypes.PLANET));
+        System.out.println(body.getKey());
 
         // test that pluto can be duplicated
         System.out.println(temp.equals(pluto));
--- Section_11_Sets&Hash_Sets/src/com/company/HeavenlyBody.java
+++ Section_11_Sets&Hash_Sets/src/com/company/HeavenlyBody.java
@@ -4,49 +4,46 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class HeavenlyBody {
-    private final String name;
+    private final Key key;
     private final double orbitalPeriod;
     private final Set<HeavenlyBody> satellites;
-    private final BodyTypes bodyTypes;
-
     public enum BodyTypes {STAR, PLANET, MOON, DWARF_PLANET};
+
     public HeavenlyBody(String name, double orbitalPeriod, BodyTypes bodyTypes) {
-        this.name = name;
         this.orbitalPeriod = orbitalPeriod;
-        this.bodyTypes = bodyTypes;
+        this.key = new Key(name, bodyTypes);
 
         this.satellites = new HashSet<>();
-    }
-
-    public String getName() {
-        return name;
     }
 
     public double getOrbitalPeriod() {
         return orbitalPeriod;
     }
 
-    public BodyTypes getOrbitalType() {
-        return bodyTypes;
-    }
-
     public Set<HeavenlyBody> getSatellites() {
         return new HashSet<>(this.satellites);
+    }
+
+    public Key getKey() {
+        return key;
     }
 
     public boolean addSatellites(HeavenlyBody moon) {
         return this.satellites.add(moon);
     }
 
+    public static Key makeKey(String name, BodyTypes bodyTypes) {
+        return new Key(name, bodyTypes);
+    }
+
     @Override
     public String toString() {
-        return "Planet " + this.name + " => " + this.orbitalPeriod;
+        return "Planet " + this.key.getName() + " => " + this.orbitalPeriod;
     }
 
     @Override
     public int hashCode() {
-        System.out.println(this.name + " hashcode: " + (this.name.hashCode() + 57));
-        return this.name.hashCode() + 57;
+        return this.key.hashCode();
     }
 
     @Override
@@ -56,11 +53,45 @@ public class HeavenlyBody {
         }
 
         if (obj instanceof HeavenlyBody) {
-            if (this.name.equals(((HeavenlyBody) obj).getName())) {
-                return this.bodyTypes == ((HeavenlyBody) obj).bodyTypes;
-            }
+            Key key = ( (HeavenlyBody) obj).getKey();
+            return this.key.equals(key);
         }
 
         return false;
+    }
+
+    public static final class Key {
+        public final String name;
+        public final BodyTypes bodyTypes;
+
+        private Key(String name, BodyTypes bodyTypes) {
+            this.name = name;
+            this.bodyTypes = bodyTypes;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public BodyTypes getBodyTypes() {
+            return bodyTypes;
+        }
+
+        @Override
+        public int hashCode() {
+            return this.name.hashCode() + 57 + this.bodyTypes.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof HeavenlyBody) {
+                String theObjectName = ((Key) obj). getName();
+                if (this.name.equals(theObjectName)) {
+                    return this.bodyTypes == ((Key) obj).getBodyTypes();
+                }
+            }
+
+            return false;
+        }
     }
 }
