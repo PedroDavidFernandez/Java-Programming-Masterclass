d60be224cfe2e927fd209c4b8426100b5d98b110
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-06-01 18:50:34 +0200

    Sorted Collections - sell items

--- SortedCollections/src/com/company/Main.java
+++ SortedCollections/src/com/company/Main.java
@@ -18,6 +18,8 @@ public class Main {
 
         temp = new StockItem("cup", 60,74);
         stockList.addStock(temp);
+        temp = new StockItem("cup", 66,90);
+        stockList.addStock(temp);
 
         temp = new StockItem("door", 72.95,4);
         stockList.addStock(temp);
@@ -35,5 +37,40 @@ public class Main {
         stockList.addStock(temp);
 
         System.out.println(stockList);
+        for (String s : stockList.Items().keySet()) {
+            System.out.println(s);
+        }
+
+        Basket basket = new Basket("Pedro's");
+        sellItem(basket, "car", 1);
+        System.out.println(basket);
+
+        sellItem(basket, "car", 1);
+        System.out.println(basket);
+
+        if (sellItem(basket, "car", 1) != 1) {
+            System.out.println("There are no more cars in stock!");
+        };
+
+        sellItem(basket, "fan", 12);
+        System.out.println(basket);
+
+        sellItem(basket, "juice", 12);
+        System.out.println(basket);
+
+        System.out.println(stockList);
+    }
+
+    public static int sellItem(Basket basket, String item, int quantity) {
+        StockItem stockItem = stockList.get(item);
+        if (stockItem == null) {
+            System.out.println("We dont sell " + item);
+            return 0;
+        }
+        if (stockList.sellStock(item, quantity) != 0) {
+            basket.addToBasket(stockItem,  quantity);
+            return quantity;
+        }
+        return 0;
     }
 }
