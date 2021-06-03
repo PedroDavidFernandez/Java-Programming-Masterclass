f67af8ad59afa77f668b8f3f388b073b49d0a253
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-06-03 19:40:57 +0200

    OCA - chapter 1: comments

---
+++ OCA/src/com/company/Chapter1.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Chapter1 {
+    public static void main(String[] args) {
+        // comments
+
+        /*
+        * // anteater
+         */
+
+        // bear
+        // // cat
+
+        // /* dog */
+        /* elephant */
+
+        
+
+        System.out.println("*** COMPILED WITH NO ERRORS ***");
+    }
+}
