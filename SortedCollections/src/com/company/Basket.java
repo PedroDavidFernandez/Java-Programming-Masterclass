38578ea2cc473297691468e2aec0e758abb6cf9c
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-06-07 12:37:31 +0200

    Sorted Collections - Basket practice

--- SortedCollections/src/com/company/Basket.java
+++ SortedCollections/src/com/company/Basket.java
@@ -1,20 +1,21 @@
 package com.company;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class Basket {
     private final String name;
-    private final Map<StockItem, Integer> list;
+    private final Map<StockItem,Integer> list;
 
-    public Basket(String name) {
+    public Basket(String name, Map<String, Integer> list) {
         this.name = name;
-        this.list = new TreeMap<>();
+        this.list = new HashMap<>();
     }
 
     public int addToBasket(StockItem item, int quantity) {
-        if ((item != null) && (quantity > 0)) {
+        if ((item != null) && (quantity >0)) {
             int inBasket = list.getOrDefault(item, 0);
             list.put(item, inBasket + quantity);
             return inBasket;
@@ -28,12 +29,12 @@ public class Basket {
 
     @Override
     public String toString() {
-        String s = "\nShopping basket " + name + " contains " + list.size() + " items\n";
+        String s = "\nShopping Basket\n" + name + " contains " + list.size() + " items\n";
         double totalCost = 0.0;
-        for (Map.Entry<StockItem, Integer> item: list.entrySet()) {
+        for (Map.Entry<StockItem, Integer> item : list.entrySet()) {
             s = s + item.getKey() + ". " + item.getValue() + " purchased\n";
             totalCost += item.getKey().getPrice() * item.getValue();
         }
-        return s + " Total cost " + totalCost;
+        return s + "Total cost " + totalCost;
     }
 }
