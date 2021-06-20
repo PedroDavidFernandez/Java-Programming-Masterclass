5def6a2ee62f549a62da75ed8e90843eb89b56d4
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-06-20 12:53:47 +0200

    Concurrency

---
+++ Concurrency/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(ThreadColor.ANSI_PURPLE + "Hello from the main thread");
+
+        Thread anotherThread = new AnotherThread();
+        anotherThread.start();
+
+        new Thread() {
+            public void run() {
+                System.out.println(ThreadColor.ANSI_GREEN + "Hello from the anonymous class Thread");
+            }
+        }.start();
+
+        System.out.println(ThreadColor.ANSI_PURPLE + "Hello again from the main Thread");
+
+        // anotherThread.start(); -> this will create an IllegalThreadStateException since instance cannot be reused
+
+    }
+}
