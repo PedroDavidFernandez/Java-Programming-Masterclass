afe174a4dcfc0a0e4bd989086721b9a96a695980
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-06-24 18:40:58 +0200

    deadlocks, notify, and notifyAll methods

--- Concurrency_Producer_and_Consumer/src/com/company/Main.java
+++ Concurrency_Producer_and_Consumer/src/com/company/Main.java
@@ -1,8 +1,88 @@
 package com.company;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Message message = new Message();
+        (new Thread(new Writer(message))).start();
+        (new Thread(new Reader(message))).start();
+    }
+}
+
+class Message {
+    private String message;
+    private boolean empty = true;
+
+    public synchronized String read() {
+        while (empty) {
+            for (int i=0; i>=0; i++) {
+                System.out.println("I am looping the reading method -> " + i);
+            }
+        }
+        empty = true;
+        return message;
+    }
+
+    public synchronized void write(String message) {
+        while (!empty) {
+            for (int i=0; i>=0; i++) {
+                System.out.println("I am looping the writing method -> " + i);
+            }
+        }
+        empty = false;
+        this.message = message;
+    }
+}
+
+class Writer implements Runnable {
+    private Message message;
+
+    public Writer(Message message) {
+        this.message = message;
+    }
+
+    public void run() {
+        String messages[] = {
+            "Humpty Dumpty sat on a wall",
+            "Humpty Dumpty had a great fall",
+            "All the king's horses and all the king's men",
+            "Couldn't put Humpty together again"
+        };
+
+        Random random = new Random();
+
+        for (int i=0; i< messages.length; i++) {
+            message.write(messages[i]);
+            try {
+                Thread.sleep(random.nextInt(2000));
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        message.write("Finished");
+    }
+}
+
+class Reader implements Runnable {
+    private Message message;
+
+    public Reader(Message message) {
+        this.message = message;
+    }
+
+    public void run() {
+        Random random = new Random();
+
+        for (String latestMessage = message.read(); !latestMessage.equals("Finished");
+             latestMessage = message.read()) {
+            System.out.println(latestMessage);
+            try {
+                Thread.sleep(random.nextInt(2000));
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
