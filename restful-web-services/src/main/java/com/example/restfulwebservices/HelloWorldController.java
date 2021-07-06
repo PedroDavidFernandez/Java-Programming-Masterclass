8156f52a4b3500c69c1bc1cc794d017488820069
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-07-06 09:11:11 +0200

    small change

--- restful-web-services/src/main/java/com/example/restfulwebservices/HelloWorldController.java
+++ restful-web-services/src/main/java/com/example/restfulwebservices/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.example.restfulwebservices;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-    @RequestMapping(method = RequestMethod.GET, path="/hello-world")
+    @GetMapping(path="/hello-world")
     public String helloWorld() {
         return "Hello World!";
     }
