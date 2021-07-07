336d2ba4914c465ccecb117187c517b09b8c47b5
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-07-07 09:04:43 +0200

    creating user bean and user service

---
+++ restful-web-services/src/main/java/com/example/restfulwebservices/user/UserDaoService.java
@@ -0,0 +1,40 @@
+package com.example.restfulwebservices.user;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class UserDaoService {
+    private static int usersCount = 3;
+    private static List<User> users = new ArrayList<>();
+
+    static {
+        users.add(new User(1, "Helena", new Date()));
+        users.add(new User(2, "Marta", new Date()));
+        users.add(new User(3, "Paula", new Date()));
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+
+    public User save(User user) {
+        if (user.getId()==null) {
+            user.setId(++usersCount);
+        }
+        users.add(user);
+        return user;
+    }
+
+    public User findOne(int id) {
+        for (User user:users){
+            if (user.getId()==id) {
+                return user;
+            }
+        }
+        return null;
+    }
+}
