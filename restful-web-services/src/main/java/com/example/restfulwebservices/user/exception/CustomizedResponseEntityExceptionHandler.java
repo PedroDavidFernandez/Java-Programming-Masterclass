b5766f6c70358109ee548622698b5549e9c9c8e7
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-07-12 17:44:58 +0200

    remove unused import

--- restful-web-services/src/main/java/com/example/restfulwebservices/user/exception/CustomizedResponseEntityExceptionHandler.java
+++ restful-web-services/src/main/java/com/example/restfulwebservices/user/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.example.restfulwebservices.user.exception;
 
-import com.example.restfulwebservices.user.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
