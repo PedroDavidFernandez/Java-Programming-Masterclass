e86c8b41cc6fc0bcbd67203ff203ff6bb2fb5d9e
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-07-14 09:38:14 +0200

    use LocaleContextHolder instead of passing locale as a param

--- restful-web-services/src/main/java/com/example/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/example/restfulwebservices/helloworld/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.example.restfulwebservices.helloworld;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -32,8 +33,8 @@ public class HelloWorldController {
 
     @GetMapping(path="/hello-world-internazionalized")
     public String helloWorldInternazionalized(
-            @RequestHeader(name="Accept-Language", required = false) Locale locale
+            // @RequestHeader(name="Accept-Language", required = false) Locale locale
     ) {
-        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+        return messageSource.getMessage("good.morning.message", null, "Default Message", LocaleContextHolder.getLocale());
     }
 }
