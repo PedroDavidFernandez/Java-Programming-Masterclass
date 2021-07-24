ca3d328404abde04817301083d33818f486dc41b
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-07-24 13:18:56 +0200

    adding some colors to the threads for better undestanding

--- Concurrency_in_Java/src/com/company/AnotherThread.java
+++ Concurrency_in_Java/src/com/company/AnotherThread.java
@@ -1,8 +1,10 @@
 package com.company;
 
+import static com.company.ThreadColor.ANSI_BLUE;
+
 public class AnotherThread extends Thread {
     @Override
     public void run() {
-        System.out.println("Hello from AnotherThread.");
+        System.out.println(ANSI_BLUE + "Hello from AnotherThread.");
     }
 }
--- Concurrency_in_Java/src/com/company/Main.java
+++ Concurrency_in_Java/src/com/company/Main.java
@@ -1,19 +1,22 @@
 package com.company;
 
+import static com.company.ThreadColor.ANSI_GREEN;
+import static com.company.ThreadColor.ANSI_PURPLE;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello from the main thread.");
+        System.out.println(ANSI_PURPLE + "Hello from the main thread.");
 
         AnotherThread anotherThread = new AnotherThread();
         anotherThread.run();
 
         new Thread() {
             public void run() {
-                System.out.println("Hello from the anonymous class thread.");
+                System.out.println(ANSI_GREEN + "Hello from the anonymous class thread.");
             }
         }.start();
 
-        System.out.println("Hello again from the main thread.");
+        System.out.println(ANSI_PURPLE + "Hello again from the main thread.");
     }
 }
