2bff8473fe4292f9d2b40891d788233548a82118
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-09-26 12:36:03 +0200

    configure jpa and initialize data

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,12 @@
+INSERT INTO currency_exchange
+(id, currency_from, currency_to, conversion_multiple, environment)
+values(1001, 'USD', 'INR', 65, '');
+INSERT INTO currency_exchange
+(id, currency_from, currency_to, conversion_multiple, environment)
+values(1002, 'EUR', 'INR', 75, '');
+INSERT INTO currency_exchange
+(id, currency_from, currency_to, conversion_multiple, environment)
+values(1003, 'AUS', 'INR', 25, '');
+INSERT INTO currency_exchange
+(id, currency_from, currency_to, conversion_multiple, environment)
+values(1004, 'GBP', 'INR', 80, '');
