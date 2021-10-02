39c9b416a884eebd45d9f428c973e7e1531cd035
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-10-02 12:05:37 +0200

    playing with Resilience4j - waitDuration

--- currency-exchange-service/src/main/java/com/example/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/example/currencyexchangeservice/CircuitBreakerController.java
@@ -14,7 +14,7 @@ public class CircuitBreakerController {
     private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
     @GetMapping("/sample-api")
-    @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+    @Retry(name = "sample-api")
     public String sampleApi() {
         logger.info("Sample API call received.");
         ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
