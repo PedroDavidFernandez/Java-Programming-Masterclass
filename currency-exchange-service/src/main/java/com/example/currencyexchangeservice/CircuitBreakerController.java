9fb3a2f30e2683002eb08c116ccdd52fff250b07
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-10-02 11:30:56 +0200

    playing with Resilience4j - max number of retries

--- currency-exchange-service/src/main/java/com/example/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/example/currencyexchangeservice/CircuitBreakerController.java
@@ -1,14 +1,24 @@
 package com.example.currencyexchangeservice;
 
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CircuitBreakerController {
 
-    @GetMapping("/sample-api")
-    public String sampleApi() {
+    private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
-        return "Sample API";
+    @GetMapping("/sample-api")
+    @Retry(name = "sample-api")
+    public String sampleApi() {
+        logger.info("Sample API call received.");
+        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
+                String.class);
+        return forEntity.getBody();
     }
 }
