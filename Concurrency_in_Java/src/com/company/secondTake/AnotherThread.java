e2ae9141b1f4694edcc74ac2aa04c2f715ab60e9
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-11-01 11:39:46 +0100

    concurrency - Runnable and Thread 2

--- Concurrency_in_Java/src/com/company/secondTake/AnotherThread.java
+++ Concurrency_in_Java/src/com/company/secondTake/AnotherThread.java
@@ -6,5 +6,13 @@ public class AnotherThread extends Thread {
     @Override
     public void run() {
         System.out.println(ANSI_BLUE + "Hello from " + currentThread().getName());
+
+        try {
+            Thread.sleep(3000);
+        } catch (Exception e) {
+            System.out.println(ANSI_BLUE + "Another Thread wake me up");
+        }
+
+        System.out.println(ANSI_BLUE + "Three seconds have passed and I am awake");
     }
 }
