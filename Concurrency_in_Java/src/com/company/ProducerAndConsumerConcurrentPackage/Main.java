0134dd8c55769d86b27085dfbcdff99d007eabfe
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-11-07 13:49:45 +0100

    concurrency - Thread Interference

--- Concurrency_in_Java/src/com/company/ProducerAndConsumerConcurrentPackage/Main.java
+++ Concurrency_in_Java/src/com/company/ProducerAndConsumerConcurrentPackage/Main.java
@@ -37,7 +37,9 @@ class MyProducer implements Runnable {
         for (String num: nums) {
             try {
                 System.out.println(color + "Adding..." + num);
-                buffer.add(num);
+                synchronized (buffer) {
+                    buffer.add(num);
+                }
 
                 Thread.sleep(random.nextInt(3000));
             } catch (InterruptedException e) {
@@ -46,7 +48,9 @@ class MyProducer implements Runnable {
         }
 
         System.out.println(color + "Adding EOF and existing...");
-        buffer.add(EOF);
+        synchronized (buffer) {
+            buffer.add(EOF);
+        }
     }
 }
 
@@ -62,15 +66,17 @@ class MyConsumer implements Runnable {
     public void run() {
         System.out.println(color + "MyConsumer run() method");
         while (true) {
-            if (buffer.isEmpty()) {
-                continue;
-            }
+            synchronized (buffer) {
+                if (buffer.isEmpty()) {
+                    continue;
+                }
 
-            if (buffer.get(0).equals(EOF)) {
-                System.out.println(color + "Exiting");
-                break;
-            } else {
-                System.out.println(color + "Removed " + buffer.remove(0));
+                if (buffer.get(0).equals(EOF)) {
+                    System.out.println(color + "Exiting");
+                    break;
+                } else {
+                    System.out.println(color + "Removed " + buffer.remove(0));
+                }
             }
         }
     }
