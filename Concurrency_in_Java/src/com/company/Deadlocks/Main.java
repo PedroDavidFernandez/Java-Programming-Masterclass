cc442f4f3e982a546b5e704f7690e755935ff99c
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-11-14 12:05:26 +0100

    concurrency - Deadlocks - solve by updating Thread 2 to obtain locks on same order than Thread 1

--- Concurrency_in_Java/src/com/company/Deadlocks/Main.java
+++ Concurrency_in_Java/src/com/company/Deadlocks/Main.java
@@ -30,20 +30,20 @@ public class Main {
 
     private static class ThreadTwo extends Thread {
         public void run() {
-            synchronized (lock2) {
-                System.out.println("Thread Two: Lock 2 obtained");
+            synchronized (lock1) {
+                System.out.println("Thread Two: Lock 1 obtained");
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
 
                 }
-                System.out.println("Thread Two: Waiting for lock 1");
-                synchronized (lock1) {
-                    System.out.println("Thead Two: lock 2 and lock 1 obtained");
+                System.out.println("Thread Two: Waiting for lock 2");
+                synchronized (lock2) {
+                    System.out.println("Thead Two: lock 1 and lock 2 obtained");
                 }
-                System.out.println("Thread One: exiting lock 1");
+                System.out.println("Thread Two: exiting lock 2");
             }
-            System.out.println("Thread One: exiting lock 2");
+            System.out.println("Thread Two: exiting lock 1");
         }
     }
 }
