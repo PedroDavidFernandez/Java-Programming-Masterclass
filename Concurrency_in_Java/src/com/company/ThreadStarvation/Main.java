37dbede1a48720802c3aeeae7318183ed235d4c5
Pedro David Fernandez Barrero <pedrodavidfernandezbarrero@gmail.com>
2021-11-21 17:20:42 +0100

    concurrency - Thread Starvation - take 2

--- Concurrency_in_Java/src/com/company/ThreadStarvation/Main.java
+++ Concurrency_in_Java/src/com/company/ThreadStarvation/Main.java
@@ -4,11 +4,11 @@ public class Main {
     private static Object lock = new Object();
 
     public static void main(String[] args) {
-        Thread t1 = new Thread(new Worker(ThreadColor.ANSI_RED), "Priority 10");
-        Thread t2 = new Thread(new Worker(ThreadColor.ANSI_BLUE), "Priority 8");
-        Thread t3 = new Thread(new Worker(ThreadColor.ANSI_GREEN), "Priority 6");
-        Thread t4 = new Thread(new Worker(ThreadColor.ANSI_PURPLE), "Priority 4");
-        Thread t5 = new Thread(new Worker(ThreadColor.ANSI_CYAN), "Priority 2");
+        Thread t1 = new Thread(new Worker(ThreadColor.BLUE_BOLD), "Priority 1");
+        Thread t2 = new Thread(new Worker(ThreadColor.CYAN_BOLD), "Priority 2");
+        Thread t3 = new Thread(new Worker(ThreadColor.GREEN_BOLD), "Priority 3");
+        Thread t4 = new Thread(new Worker(ThreadColor.PURPLE_BOLD), "Priority 4");
+        Thread t5 = new Thread(new Worker(ThreadColor.YELLOW_BRIGHT), "Priority 5");
 
         t1.setPriority(10);
         t2.setPriority(8);
@@ -23,9 +23,9 @@ public class Main {
         t5.start();
     }
 
-    private static class Worker implements Runnable {
-        private int countRunner = 1;
+    private static class Worker implements Runnable{
         private String threadColor;
+        private int countRunner = 0;
 
         public Worker(String threadColor) {
             this.threadColor = threadColor;
@@ -34,11 +34,10 @@ public class Main {
         @Override
         public void run() {
             synchronized (lock) {
-                for(int i= 0; i<100; i++) {
-                    System.out.format(threadColor + "%s: count is:%d\n",
-                            Thread.currentThread().getName(), countRunner++);
+                for (int i=0; i<100; i++) {
+                    System.out.format(
+                            threadColor + "%s:count runner %d\n", Thread.currentThread().getName(), ++countRunner);
                 }
-                // critical area of code
             }
         }
     }
