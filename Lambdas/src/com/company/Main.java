8afe1a14576c186ace66aa097019d29f0a39b3f2
Pedro David Fernandez Barrero <molders.smileys.0x@icloud.com>
2021-11-24 20:04:51 +0100

    lambdas - lambda expression - compiler error

--- Lambdas/src/com/company/Main.java
+++ Lambdas/src/com/company/Main.java
@@ -3,9 +3,11 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        new Thread(()-> {
-            System.out.println("Printing from the Lamba expression");
-        }).start();
+        new Thread(()->
+                System.out.println("Printing from the Lamba expression");
+                System.out.println("Printing from the Lamba expression");
+        ).start();
+
     }
 
 }
