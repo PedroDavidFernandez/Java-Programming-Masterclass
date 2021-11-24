57376e8b8cf635e15c430b1011cbf81df56cb53e
Pedro David Fernandez Barrero <molders.smileys.0x@icloud.com>
2021-11-24 19:52:00 +0100

    lambdas - printing from the anonymous class

--- Lambdas/src/com/company/Main.java
+++ Lambdas/src/com/company/Main.java
@@ -3,7 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    new Thread(new CodeThreat()).start();
+	    new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Printing from the anonymous class");
+            }
+        }).start();
     }
 }
 
