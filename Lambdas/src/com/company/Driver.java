715a15fe02fd14ab467651c7b50890531bd5501c
Pedro David Fernandez Barrero <molders.smileys.0x@icloud.com>
2021-11-26 09:30:19 +0100

    lambdas - lambda expressions - anonyous class overrides interface

--- Lambdas/src/com/company/Driver.java
+++ Lambdas/src/com/company/Driver.java
@@ -23,7 +23,24 @@ public class Driver {
         for (Student student : students) {
             System.out.println(student.getName() + " is " + student.getAge());
         }
+
+        String sillyString = doStringStuff(new UpperConcat() {
+            @Override
+            public String upperAndConcat(String s1, String s2) {
+                return s1.toUpperCase() + s2.toUpperCase();
+            }
+        },
+        students.get(0).getName(), students.get(1).getName());
+        System.out.println(sillyString);
     }
+
+    public final static String doStringStuff(UpperConcat uc, String s1, String s2) {
+        return uc.upperAndConcat(s1, s2);
+    }
+}
+
+interface UpperConcat{
+    public String upperAndConcat(String s1, String s2);
 }
 
 class Student{
