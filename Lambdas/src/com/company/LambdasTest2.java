0df2a6acf3aed150acee6f045e57660c97a6bf12
Pedro David Fernandez Barrero <molders.smileys.0x@icloud.com>
2021-12-04 14:05:16 +0100

    lambdas - Lambda example from Thread: anonymous class

--- Lambdas/src/com/company/LambdasTest2.java
+++ Lambdas/src/com/company/LambdasTest2.java
@@ -2,7 +2,11 @@ package com.company;
 
 public class LambdasTest2 {
     public static void main(String[] args) {
-        Thread t1 = new Thread( () -> System.out.println("I am running from the Lambda Expression"));
+        Thread t1 = new Thread(){
+            public void run() {
+                System.out.println("Running from the anonymous class");
+            }
+        };
         t1.start();
     }
 }
